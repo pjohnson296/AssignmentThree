17f8b9d830a3a12c49c47ed98da518ce14e3097b
Alex Peterson <petestreet01@gmail.com>
2014-09-11 12:02:13 -0500

    added the second page

--- src/com/example/assignmentthree/MainActivity.java
+++ src/com/example/assignmentthree/MainActivity.java
@@ -3,9 +3,11 @@
 package com.example.assignmentthree;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 
 public class MainActivity extends Activity {
@@ -35,4 +37,9 @@ public class MainActivity extends Activity {
         }
         return super.onOptionsItemSelected(item);
     }
+    
+	public void nextPage(View view) {
+		Intent intent = new Intent(this, SecondPageActivity.class);
+		startActivity(intent);
+	}
 }
